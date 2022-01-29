c7f7ae6a499d0c606354d3ada7d5f1b8d126bf37
Ndibechinonso <edwardndibe@gmail.com>
2022-01-29 21:07:31 +0100

    res options

--- controllers/userController.js
+++ controllers/userController.js
@@ -72,7 +72,7 @@ const authenticateUser = (req, res, next) => {
                     const token = jwt.sign(payload, secret, {
                         expiresIn: '1h'
                     });
-                    res.cookie('token', [token,payload] , { httpOnly: true }).sendStatus(200);
+                    res.cookie('token', [token, payload]).sendStatus(200);
                     // res.cookie('email', payload, { httpOnly: true }).sendStatus(200);
                 }
             });
